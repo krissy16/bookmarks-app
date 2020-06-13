e31e68d96e898ce3493dbfe6d9732f4ca7c08c2c
Krissy <krissysaenz@gmail.com>
2020-06-13 16:01:57 -0700

    context, edit and more

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
+console.log(process.env.REACT_APP_API_KEY)
 export default {
-  API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+dotenv.config()
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
