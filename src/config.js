d4ddff2668c4a248cb267fd992ee0b48a3da8226
Krissy <krissysaenz@gmail.com>
2020-06-19 16:25:28 -0700

    updates production api url and fixes delete

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-console.log(process.env.REACT_APP_API_KEY)
 export default {
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  API_ENDPOINT: `https://young-tor-70906.herokuapp.com/api/bookmarks`,
+  //API_ENDPOINT: `http://localhost:8000`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
